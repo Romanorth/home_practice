1e73165faab954058f64dd77d6d816e81c195da3
Romanorth <hemnorotupo@mail.ru>
2022-03-25 21:05:37 +0300

    Added empty urls and methods

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
+
+def index(request):
+    return HttpResponse('My response')
+
+
+def group_posts(request, any_slug):
+    return HttpResponse('My group response ' + any_slug)
 
 # Create your views here.
